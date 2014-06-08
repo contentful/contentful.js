8ec3805078c03e9320faaaf4e5a95effba05aeb8
pawelrychlik <pawel.rychlik@rspective.pl>
2014-06-08 18:09:39 +0200

    added missing dependency

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var _ = require('underscore-contrib');
 var questor = require('questor');
 var redefine = require('redefine');
 var resolveResponse = require('contentful-resolve-response');
