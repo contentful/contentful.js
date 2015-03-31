be7083ae9914730603c5221ed45cf8d54fa797cf
Stephen Sugden <me@stephensugden.com>
2015-03-31 19:47:22 +0200

    Convert to ES6 syntax

--- index.es6.js
+++ index.es6.js
@@ -6,8 +6,12 @@ var redefine = require('redefine');
 var resolveResponse = require('contentful-resolve-response');
 var querystring = require('querystring');
 
-var Client = redefine.Class({
-  constructor: function Client(options) {
+export default function createClient (options) {
+  return new Client(options || {});
+};
+
+class Client {
+  constructor (options) {
     enforcep(options, 'accessToken');
     enforcep(options, 'space');
 
@@ -15,9 +19,9 @@ var Client = redefine.Class({
       host: 'cdn.contentful.com',
       secure: true
     });
-  },
+  }
 
-  _request: function(path, query) {
+  _request (path, query) {
     if (!query) query = {};
     query.access_token = this.options.accessToken;
 
@@ -46,53 +50,53 @@ var Client = redefine.Class({
       .catch(function (error) {
         throw error.data;
       });
-  },
+  }
 
-  asset: function(id, callback) {
+  asset (id, callback) {
     return nodeify(
       this._request('/assets/' + id).then(Asset.parse),
       callback
     );
-  },
+  }
 
-  assets: function(object, callback) {
+  assets (object, callback) {
     var query = Query.parse(object);
     var deferred = this._request('/assets', query)
                    .then(_.partial(SearchResult.parse));
     return nodeify(deferred, callback);
-  },
+  }
 
-  contentType: function(id, callback) {
+  contentType (id, callback) {
     var deferred = this._request('/content_types/' + id)
                    .then(ContentType.parse);
     return nodeify(deferred, callback);
-  },
+  }
 
-  contentTypes: function(object, callback) {
+  contentTypes (object, callback) {
     var query = Query.parse(object);
     var deferred = this._request('/content_types', query)
                    .then(_.partial(SearchResult.parse));
     return nodeify(deferred, callback);
-  },
+  }
 
-  entry: function(id, callback) {
+  entry (id, callback) {
     var deferred = this._request('/entries/' + id)
                    .then(Entry.parse);
     return nodeify(deferred, callback);
-  },
+  }
 
-  entries: function(object, callback) {
+  entries (object, callback) {
     var query = Query.parse(object);
     var deferred = this._request('/entries', query)
                    .then(_.partial(SearchResult.parse));
     return nodeify(deferred, callback);
-  },
+  }
 
-  space: function(callback) {
+  space (callback) {
     return nodeify(this._request(''), callback);
-  },
+  }
 
-  _pagedSync: function (sync) {
+  _pagedSync  (sync) {
     var self = this;
     return this._request('/sync', sync.query)
                .then(function (data) {
@@ -106,9 +110,9 @@ var Client = redefine.Class({
                    };
                  }
                });
-  },
+  }
 
-  sync: function (object, callback) {
+  sync  (object, callback) {
     if (!object || (!object.initial && !object.nextSyncToken)) {
       throw new Error('Please provide either the initial flag or a nextSyncToken for syncing');
     }
@@ -120,141 +124,105 @@ var Client = redefine.Class({
                    });
     return nodeify(deferred, callback);
   }
-});
+}
 
-var Asset = redefine.Class({
-  constructor: function Asset() {},
-
-  statics: {
-    parse: function(object) {
-      return _.extend(new Asset(), {
-        sys: Sys.parse(object.sys),
-        fields: object.fields
-      });
-    }
+class Asset {
+  static parse (object) {
+    return _.extend(new Asset(), {
+      sys: Sys.parse(object.sys),
+      fields: object.fields
+    });
   }
-});
+}
 
-var Entry = redefine.Class({
-  constructor: function Entry() {},
-
-  statics: {
-    parse: function(object) {
-      return _.extend(new Entry(), {
-        sys: Sys.parse(object.sys),
-        fields: object.fields
-      });
-    }
+class Entry {
+  static parse (object) {
+    return _.extend(new Entry(), {
+      sys: Sys.parse(object.sys),
+      fields: object.fields
+    });
   }
-});
+}
 
-var ContentType = redefine.Class({
-  constructor: function ContentType() {},
-
-  statics: {
-    parse: function(object) {
-      return _.extend(new ContentType(), {
-        sys: Sys.parse(object.sys),
-        fields: object.fields.map(Field.parse),
-      }, _.pick(object, 'name', 'displayField'));
-    }
+class ContentType {
+  static parse (object) {
+    return _.extend(new ContentType(), {
+      sys: Sys.parse(object.sys),
+      fields: object.fields.map(Field.parse),
+    }, _.pick(object, 'name', 'displayField'));
   }
-});
+}
 
-var Field = redefine.Class({
-  constructor: function Field() {},
-
-  statics: {
-    parse: function(object) {
-      return _.extend(new Field(), object);
-    }
+class Field {
+  static parse (object) {
+    return _.extend(new Field(), object);
   }
-});
+}
 
-var SearchResult = redefine.Class({
-  constructor: function SearchResult() {},
-
-  statics: {
-    parse: function(object) {
-      walkMutate(object, isParseableResource, parseResource);
-      var items = resolveResponse(object);
-      return redefine(
-        items, {
-          limit: object.limit,
-          skip: object.skip,
-          total: object.total
-        }, {
-          enumerable: false
-        }
-      );
-    }
+class SearchResult {
+  static parse (object) {
+    walkMutate(object, isParseableResource, parseResource);
+    var items = resolveResponse(object);
+    return redefine(
+      items, {
+        limit: object.limit,
+        skip: object.skip,
+        total: object.total
+      }, {
+        enumerable: false
+      }
+    );
   }
-});
+}
 
-var Query = redefine.Class({
-  constructor: function Query() {},
-
-  toQueryString: function() {
+class Query {
+  toQueryString () {
     return querystring.stringify(this);
-  },
-
-  statics: {
-    parse: function(object) {
-      return _.extend(new Query(), stringifyArrayValues(object));
-    },
   }
-});
 
-var Space = redefine.Class({
-  constructor: function Space() {},
-
-  statics: {
-    parse: function(object) {
-      return _.extend(new Space(), object);
-    }
+  static parse (object) {
+    return _.extend(new Query(), stringifyArrayValues(object));
   }
-});
+}
 
-var Sys = redefine.Class({
-  constructor: function Sys() {},
-
-  statics: {
-    parse: function(object) {
-      return _.extend(
-        new Sys(),
-        _.pick(object, 'id', 'revision', 'type', 'locale'),
-        compacto({
-          contentType: object.contentType && Link.parse(object.contentType),
-          createdAt: object.createdAt && new Date(object.createdAt),
-          linkType: object.linkType,
-          updatedAt: object.updatedAt && new Date(object.updatedAt),
-          space: object.space && Link.parse(object.space)
-        })
-      );
-    }
+class Space {
+  static parse (object) {
+    return _.extend(new Space(), object);
   }
-});
+}
 
-var Link = redefine.Class({
-  constructor: function Link() {},
-
-  statics: {
-    parse: function(object) {
-      return _.extend(new Link(), {
-        sys: Sys.parse(object.sys)
-      });
-    }
+class Sys {
+  static parse (object) {
+    return _.extend(
+      new Sys(),
+      _.pick(object, 'id', 'revision', 'type', 'locale'),
+      compacto({
+        contentType: object.contentType && Link.parse(object.contentType),
+        createdAt: object.createdAt && new Date(object.createdAt),
+        linkType: object.linkType,
+        updatedAt: object.updatedAt && new Date(object.updatedAt),
+        space: object.space && Link.parse(object.space)
+      })
+    );
   }
-});
+}
 
-var Sync = redefine.Class({
-  constructor: function Sync(query) {
+class Link {
+  static parse (object) {
+    return _.extend(new Link(), {
+      sys: Sys.parse(object.sys)
+    });
+  }
+}
+
+class Sync {
+  constructor (query) {
     this.query = query;
     this.items = [];
     this.done = false;
-  },
+  }
 
-  append: function (data) {
+  append (data) {
     this.items = this.items.concat(data.items);
     if(data.nextPageUrl){
       var nextPageUrl = data.nextPageUrl.split('?');
@@ -266,13 +234,8 @@ var Sync = redefine.Class({
       this.done = true;
     }
   }
+}
 
-});
-
-
-exports.createClient = function(options) {
-  return new Client(options || {});
-};
 
 function exists(value) {
   return value != null;
