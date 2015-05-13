7293bd9c413de1e57fc45aa25d20aea2f416f91d
Thomas Strothjohann <thomas@strothjohann.net>
2015-05-13 14:03:27 +0200

    Added google map

--- example/ufos/index.js
+++ example/ufos/index.js
@@ -11,6 +11,26 @@ var client = contentful.createClient({
   accessToken: '0c6ef483524b5e46b3bafda1bf355f38f5f40b4830f7599f790a410860c7c271'
 });
 
+// Google map
+var map;
+var mapStyles = [{"featureType":"administrative.locality","elementType":"all","stylers":[{"hue":"#2c2e33"},{"saturation":7},{"lightness":19},{"visibility":"on"}]},{"featureType": "poi","stylers": [{"visibility": "off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#bbc0c4"},{"saturation":-53},{"lightness":31}]},{"featureType":"road","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"on"}]},{"featureType":"road.arterial","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":-2},{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"hue":"#e9ebed"},{"saturation":-90},{"lightness":-8},{"visibility":"simplified"}]},{"featureType":"transit"},{"featureType":"water","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":-78},{"lightness":67},{"visibility":"simplified"}]}];
+var mapOptions = {
+    zoom: 4,
+    center: {lat: 35.3732921, lng: -119.0187125},
+    panControl: false,
+    mapTypeControl: false,
+    streetViewControl: false,
+    overviewMapControl: false,
+    styles: mapStyles
+};
+
+function initializeMap() {
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initializeMap);
+
+// Angular controller
 function UfoController($scope, $timeout, enQ) {
   var query = $scope.query = {
     term: ''
@@ -84,7 +104,7 @@ function ufoToGeoJson(ufo) {
     },
     properties: {
       title: ufo.fields.locationName,
-      'marker-color': '#fc3159',
+      // 'marker-color': '#fc3159',
       ufo: ufo
     }
   };
@@ -148,4 +168,3 @@ angular.module('ufos', []).
       return deferred.promise;
     };
   });
-
