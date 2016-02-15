7e1344ddcd4b810f6b6a8ed0feee47ecbc93490d
Tiago Rodrigues <tmcrodrigues@gmail.com>
2016-02-15 13:53:44 +0100

    feat: Link getters can resolve arrays of links

--- test/unit/mixins/link-getters-test.js
+++ test/unit/mixins/link-getters-test.js
@@ -1,117 +1,110 @@
 import test from 'tape'
 import mixinLinkGetters from '../../../lib/mixins/link-getters'
 
-test('empty response', t => {
-  const response = {items: []}
-  t.looseEquals(mixinLinkGetters(response), response)
-  t.end()
-})
-
-test('no links in response', t => {
-  const response = {
-    items: [{
-      foo: 'bar'
-    }]
-  }
-  const mixedResponse = mixinLinkGetters(response)
-  t.looseEquals(mixedResponse, response)
-  t.end()
-})
-
 test('links in response, without matching include should remain', t => {
-  const response = {
-    items: [
-      {sys: {type: 'Link', linkType: 'Piglet', id: 'oink'}
-      }]
-  }
-  const mixedResponse = mixinLinkGetters(response)
-  t.looseEquals(mixedResponse, response)
+  const items = [{
+    sys: {type: 'Entry', locale: 'en-US'},
+    fields: {
+      animal: {sys: {type: 'Link', linkType: 'Piglet', id: 'oink'}}
+    }
+  }]
+  mixinLinkGetters(items, {})
+  t.equal(items[0].fields.animal.sys.type, 'Link')
   t.end()
 })
 
 test('links in response, with matching include should resolve', t => {
-  const response = {
-    items: [
+  const items = [
+    {
+      sys: {type: 'Entry', locale: 'en-US'},
+      fields: {
+        animal: {sys: {type: 'Link', linkType: 'Animal', id: 'oink'}},
+        anotheranimal: {sys: {type: 'Link', linkType: 'Animal', id: 'middle-parrot'}}
+      }
+    },
+    {
+      sys: {type: 'Entry', locale: 'en-US'},
+      fields: {
+        birds: [
+          {sys: {type: 'Link', linkType: 'Animal', id: 'parrot'}},
+          {sys: {type: 'Link', linkType: 'Animal', id: 'middle-parrot'}},
+          {sys: {type: 'Link', linkType: 'Animal', id: 'aussie-parrot'}}
+        ]
+      }
+    },
+    {
+      sys: {type: 'Entry'},
+      fields: {
+        animal: {
+          'en-US': {sys: {type: 'Link', linkType: 'Animal', id: 'oink'}}
+        },
+        animals: {
+          'en-US': [{sys: {type: 'Link', linkType: 'Animal', id: 'oink'}}]
+        }
+      }
+    }
+  ]
+  const includes = {
+    Animal: [
       {
-        sys: {type: 'Entry'},
+        sys: {type: 'Animal', id: 'oink', locale: 'en-US'},
         fields: {
-          animal: {sys: {type: 'Link', linkType: 'Animal', id: 'oink'}},
-          anotheranimal: {sys: {type: 'Link', linkType: 'Animal', id: 'middle-parrot'}}
+          name: 'Pig',
+          friend: {sys: {type: 'Link', linkType: 'Animal', id: 'groundhog'}}
         }
       },
       {
-        sys: {type: 'Entry'},
-        fields: {
-          birds: [
-            {sys: {type: 'Link', linkType: 'Animal', id: 'parrot'}},
-            {sys: {type: 'Link', linkType: 'Animal', id: 'middle-parrot'}},
-            {sys: {type: 'Link', linkType: 'Animal', id: 'aussie-parrot'}}
-          ]
-        }
+        sys: {type: 'Animal', id: 'groundhog', locale: 'en-US'},
+        fields: {name: 'Phil'}
+      },
+      {
+        sys: {type: 'Animal', id: 'parrot', locale: 'en-US'},
+        fields: {name: 'Parrot'}
+      },
+      {
+        sys: {type: 'Animal', id: 'aussie-parrot', locale: 'en-US'},
+        fields: {name: 'Aussie Parrot'}
       }
-    ],
-    includes: {
-      Animal: [
-        {
-          sys: {type: 'Animal', id: 'oink'},
-          fields: {
-            name: 'Pig',
-            friend: {sys: {type: 'Link', linkType: 'Animal', id: 'groundhog'}}
-          }
-        },
-        {
-          sys: {type: 'Animal', id: 'groundhog'},
-          fields: {name: 'Phil'}
-        },
-        {
-          sys: {type: 'Animal', id: 'parrot'},
-          fields: {name: 'Parrot'}
-        },
-        {
-          sys: {type: 'Animal', id: 'aussie-parrot'},
-          fields: {name: 'Aussie Parrot'}
-        }
-      ]
-    }
+    ]
   }
 
-  const mixedResponse = mixinLinkGetters(response)
-  const items = mixedResponse.items
-  t.looseEquals(items[0].fields.animal.sys, response.includes.Animal[0].sys, 'pig')
-  t.looseEquals(items[0].fields.animal.fields.friend.sys, response.includes.Animal[1].sys, 'groundhog')
+  mixinLinkGetters(items, includes)
+  t.looseEquals(items[0].fields.animal.sys, includes.Animal[0].sys, 'pig')
+  t.looseEquals(items[0].fields.animal.fields.friend.sys, includes.Animal[1].sys, 'groundhog')
   t.looseEquals(items[0].fields.anotheranimal.sys.type, 'Link', 'first middle parrot not resolved')
-  t.looseEquals(items[1].fields.birds[0], response.includes.Animal[2], 'parrot')
+  t.looseEquals(items[1].fields.birds[0], includes.Animal[2], 'parrot')
   t.looseEquals(items[1].fields.birds[1].sys.type, 'Link', 'second middle parrot not resolved')
-  t.looseEquals(items[1].fields.birds[2], response.includes.Animal[3], 'aussie parrot')
+  t.looseEquals(items[1].fields.birds[2], includes.Animal[3], 'aussie parrot')
+  t.looseEquals(items[2].fields.animal['en-US'].sys, includes.Animal[0].sys, 'localized pig')
+  t.looseEquals(items[2].fields.animal['en-US'].fields.friend.sys, includes.Animal[1].sys, 'localized groundhog')
+  t.looseEquals(items[2].fields.animals['en-US'][0].sys, includes.Animal[0].sys, 'listed localized pig')
+  t.looseEquals(items[2].fields.animals['en-US'][0].fields.friend.sys, includes.Animal[1].sys, 'listed localized groundhog')
   t.end()
 })
 
 test('links in response, with circular references', t => {
-  const response = {
-    items: [
-      {
-        sys: {type: 'Entry'},
-        fields: {
-          animal: {sys: {type: 'Link', linkType: 'Animal', id: 'oink'}}
-        }
+  const items = [
+    {
+      sys: {type: 'Entry', locale: 'en-US'},
+      fields: {
+        animal: {sys: {type: 'Link', linkType: 'Animal', id: 'oink'}}
       }
-    ],
-    includes: {
-      Animal: [
-        {
-          sys: {type: 'Animal', id: 'oink'},
-          fields: {name: 'Pig', friend: {sys: {type: 'Link', linkType: 'Animal', id: 'parrot'}}}
-        },
-        {
-          sys: {type: 'Animal', id: 'parrot'},
-          fields: {name: 'Parrot', friend: {sys: {type: 'Link', linkType: 'Animal', id: 'oink'}}}
-        }
-      ]
     }
+  ]
+  const includes = {
+    Animal: [
+      {
+        sys: {type: 'Animal', id: 'oink', locale: 'en-US'},
+        fields: {name: 'Pig', friend: {sys: {type: 'Link', linkType: 'Animal', id: 'parrot'}}}
+      },
+      {
+        sys: {type: 'Animal', id: 'parrot', locale: 'en-US'},
+        fields: {name: 'Parrot', friend: {sys: {type: 'Link', linkType: 'Animal', id: 'oink'}}}
+      }
+    ]
   }
 
-  const mixedResponse = mixinLinkGetters(response)
-  const items = mixedResponse.items
+  mixinLinkGetters(items, includes)
 
   t.equals(items[0].fields.animal.sys.type, 'Animal', 'first link type')
   t.equals(items[0].fields.animal.sys.id, 'oink', 'first link id')
