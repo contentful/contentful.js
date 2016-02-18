3349835b8be424ccb6e92c5cfc1a27587db5f11a
Tiago Rodrigues <tmcrodrigues@gmail.com>
2016-02-18 14:17:44 +0100

    chore(vendor): Vendorize axios with separate node/browser builds
    This fixes the problem where the browser build would include a lot of code from axios that isn't
    necessary for browser environments.

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+// To understand why axios is vendored, check SETUP.md
+var axios = require('./vendor-node/axios')
+var contentful
 try {
-  module.exports = require('./dist/contentful').default
+  contentful = require('./dist/contentful').default
 } catch (err) {
   if (err.code === 'MODULE_NOT_FOUND') {
     require('babel-register')
-    module.exports = require('./lib/contentful').default
+    contentful = require('./lib/contentful').default
   } else {
     console.log(err)
     process.exit(1)
+  }
+}
+
+module.exports = {
+  createClient: function (params) {
+    return contentful(axios, params)
   }
 }
