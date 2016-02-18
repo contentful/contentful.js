5c328fa3aa7a1597e6e97cae16d0106d00670de7
Tiago Rodrigues <tmcrodrigues@gmail.com>
2016-02-18 14:18:59 +0100

    chore(build): Improve and optimize browser build

--- browser.js
+++ browser.js
@@ -1,1 +1,8 @@
-module.exports = require('./lib/contentful').default
+// To understand why axios is vendored, check SETUP.md
+var axios = require('./vendor-node/axios')
+var contentful = require('./lib/contentful').default
+module.exports = {
+  createClient: function (params) {
+    return contentful(axios, params)
+  }
+}
