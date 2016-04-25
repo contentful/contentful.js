04835838f07fb5592c76534b88cf9beb22eb522a
Tiago Rodrigues <tmcrodrigues@gmail.com>
2016-04-25 13:31:08 +0200

    refactor: move api creation module back to contentful.js

--- test/runner-browser.js
+++ test/runner-browser.js
@@ -1,6 +1,7 @@
 // This file is required due to an issue with karma-tap
 // https://github.com/tmcw-up-for-adoption/karma-tap/issues/10
 require('./unit/contentful-test.js')
+require('./unit/create-contentful-api-test.js')
 require('./unit/create-sync-requester-test.js')
 require('./unit/entities/asset-test.js')
 require('./unit/entities/content-type-test.js')
