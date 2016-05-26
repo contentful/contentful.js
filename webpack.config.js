8ed6c43f8195bc2a1d0dc016fe7554e8415472d3
Tiago Rodrigues <tmcrodrigues@gmail.com>
2016-05-26 11:44:33 +0200

    fix: Fix webpack config path usage

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 var webpack = require('webpack')
+var path = require('path')
 module.exports = {
   context: __dirname,
   entry: './browser',
   output: {
-    path: __dirname + '/browser-dist',
+    path: path.join(__dirname, '/browser-dist'),
     filename: 'contentful.js',
     library: 'contentful'
   },
