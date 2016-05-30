1ebf7c2bf3dfbcde6b84c4f656524e0376f8d4a3
Tiago Rodrigues <tmcrodrigues@gmail.com>
2016-05-30 16:55:14 +0200

    chore: update saucelabs browser config

--- karma.conf.saucelabs.js
+++ karma.conf.saucelabs.js
@@ -4,25 +4,26 @@ var browsers = {
   sl_chrome: {
     base: 'SauceLabs',
     browserName: 'chrome',
-    platform: 'Windows 7',
-    version: '48'
+    platform: 'Windows 10',
+    version: '50.0'
   },
   sl_firefox: {
     base: 'SauceLabs',
     browserName: 'firefox',
-    version: '44'
+    platform: 'Windows 10',
+    version: '46.0'
   },
   sl_ie_11: {
     base: 'SauceLabs',
     browserName: 'internet explorer',
-    platform: 'Windows 8.1',
-    version: '11'
+    platform: 'Windows 10',
+    version: '11.103'
   },
   sl_edge: {
     base: 'SauceLabs',
     browserName: 'MicrosoftEdge',
     platform: 'Windows 10',
-    version: '20.10240'
+    version: '13.10586'
   }
 }
 
