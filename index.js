40f0caf060ba038dee079589f80a3a64ac9c20f4
Khaled Garbaya <khaled@contentful.com>
2016-07-07 14:25:46 +0200

    feat(contentful): Introduce createClientWithCustomHttpVendor
    Introduce a way of creating a client with custom http vendor instance

--- index.js
+++ index.js
@@ -16,5 +16,11 @@ try {
 module.exports = {
   createClient: function (params) {
     return contentful(axios, params)
+  },
+  // This is intended to be used only for debug reasons
+  // e.g: if you want to add interceptor to axios or if you want to use a different vendor
+  // if you want to use a different vendor than axios make sure it is using promises
+  createClientWithCustomHttpVendor: function (httpVendor, params) {
+    return contentful(httpVendor, params)
   }
 }
