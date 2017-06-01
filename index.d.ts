5f3f60b11e9a971e5a1131fb5761314d9faf1837
Benjamin Otto <benjamin.otto@style-systems.de>
2017-06-01 22:01:26 +0200

    Update index.d.ts
    Fixed typo

--- index.d.ts
+++ index.d.ts
@@ -43,7 +43,7 @@ export interface Asset {
         file: {
             url: string;
             details: any;
-            fileNmae: string;
+            fileName: string;
             contentType: string;
         };
     };
