def86d06811698d44c485f7ef436be24ebe6299a
Benedikt Rötsch <benedikt@contentful.com>
2017-07-25 14:22:33 +0200

    test(browser): added Firefox to local browser test

--- karma.conf.local.js
+++ karma.conf.local.js
@@ -2,7 +2,8 @@ var karmaBaseConf = require('./karma.base.conf')
 
 module.exports = function (config) {
   karmaBaseConf.plugins.push(require('karma-chrome-launcher'))
-  karmaBaseConf.browsers = ['Chrome']
+  karmaBaseConf.plugins.push(require('karma-firefox-launcher'))
+  karmaBaseConf.browsers = ['Chrome', 'Firefox']
   karmaBaseConf.logLevel = config.LOG_DEBUG
 
   config.set(karmaBaseConf)
