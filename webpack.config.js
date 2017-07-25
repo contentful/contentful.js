9063408abf5202e23eaee41723d43d891afd0d2b
Benedikt Rötsch <benedikt@contentful.com>
2017-07-25 17:25:32 +0200

    build(legacy): introduce legacy version to support IE11

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path')
 
 const webpack = require('webpack')
 const BabiliPlugin = require('babili-webpack-plugin')
+const clone = require('clone')
 
 const PROD = process.env.NODE_ENV === 'production'
 
 const plugins = [
   new webpack.optimize.OccurrenceOrderPlugin(),
-  new webpack.IgnorePlugin(/vertx/),
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
   })
@@ -25,68 +25,83 @@ if (PROD) {
   )
 }
 
-const jsLoaderIncludes = [
+const baseFileName = `contentful`
+const include = [
   path.resolve(__dirname, 'lib'),
+  path.resolve(__dirname, 'test'),
+  // Include mem and it's dependencies since they use ES6 code
   path.resolve(__dirname, 'node_modules', 'mem'),
   path.resolve(__dirname, 'node_modules', 'mimic-fn')
 ]
 
-module.exports = [
-  {
-    // Browser
-    context: path.join(__dirname, 'lib'),
-    entry: './contentful.js',
-    output: {
-      path: path.join(__dirname, 'dist'),
-      filename: `contentful${PROD ? '.min' : ''}.js`,
-      libraryTarget: 'umd',
-      library: 'contentful'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.js?$/,
-          include: jsLoaderIncludes,
-          loader: 'babel-loader',
-          options: {
-            env: 'browser'
-          }
-        }
-      ],
-      noParse: (content) => {
-        return /clone/.test(content)
-      }
-    },
-    devtool: PROD ? false : 'source-map',
-    plugins: plugins
+const baseBundleConfig = {
+  context: path.join(__dirname, 'lib'),
+  entry: ['./contentful.js'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'contentful'
   },
+  module: {
+    loaders: [],
+    noParse: (content) => {
+      return /clone/.test(content)
+    }
+  },
+  devtool: PROD ? false : 'source-map',
+  plugins
+}
+
+// Browsers
+const browserBundle = clone(baseBundleConfig)
+browserBundle.module.loaders = [
   {
-    // Node
-    context: path.join(__dirname, 'lib'),
-    entry: './contentful.js',
-    target: 'node',
-    output: {
-      path: path.join(__dirname, 'dist'),
-      filename: `contentful.node${PROD ? '.min' : ''}.js`,
-      libraryTarget: 'commonjs2',
-      library: 'contentful'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.js?$/,
-          include: jsLoaderIncludes,
-          loader: 'babel-loader',
-          options: {
-            env: 'node'
-          }
-        }
-      ],
-      noParse: (content) => {
-        return /clone/.test(content)
-      }
-    },
-    devtool: PROD ? false : 'source-map',
-    plugins: plugins
+    test: /\.js?$/,
+    include,
+    loader: 'babel-loader',
+    options: {
+      env: 'browser'
+    }
   }
+]
+browserBundle.output.filename = `${baseFileName}${PROD ? '.min' : ''}.js`
+
+// Legacy browsers like IE11
+const legacyInclude = include.slice()
+legacyInclude.push(path.resolve(__dirname, 'node_modules', 'follow-redirects')) // follow-redirects uses Object.assign
+
+const legacyBundle = clone(baseBundleConfig)
+legacyBundle.module.loaders = [
+  {
+    test: /\.js?$/,
+    include: legacyInclude,
+    loader: 'babel-loader',
+    options: {
+      env: 'legacy'
+    }
+  }
+]
+legacyBundle.entry = ['core-js/fn/promise'].concat(legacyBundle.entry) // Promise polyfill
+legacyBundle.output.filename = `${baseFileName}.legacy${PROD ? '.min' : ''}.js`
+
+// Node
+const nodeBundle = clone(baseBundleConfig)
+nodeBundle.module.loaders = [
+  {
+    test: /\.js?$/,
+    include,
+    loader: 'babel-loader',
+    options: {
+      env: 'node'
+    }
+  }
+]
+nodeBundle.target = 'node'
+nodeBundle.output.libraryTarget = 'commonjs2'
+nodeBundle.output.filename = `${baseFileName}.node${PROD ? '.min' : ''}.js`
+
+module.exports = [
+  browserBundle,
+  legacyBundle,
+  nodeBundle
 ]
