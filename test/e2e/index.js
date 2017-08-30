7b36002a509d84e2fdf568b3600a38bb7e81e425
Benedikt Rötsch <benedikt@contentful.com>
2017-08-30 11:26:42 +0200

    test(e2e): initial end to end tests

---
+++ test/e2e/index.js
@@ -0,0 +1,37 @@
+const { Builder, By, until } = require('selenium-webdriver')
+
+const initServer = require('./server')
+
+const mode = process.env.CONTENTFUL_E2E_MODE || 'browser'
+
+const caps = {
+  platform: 'Windows 10',
+  browserName: 'firefox',
+  'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+  build: process.env.TRAVIS_BUILD_NUMBER,
+  tag: 'e2e',
+  name: 'Firefox e2e test with .browser.min.js'
+}
+
+if (mode === 'legacy') {
+  caps.browserName = 'internet explorer'
+  caps.version = '11.103'
+  caps.name = 'IE 11 e2e test with .legacy.min.js'
+}
+
+const driver = new Builder()
+  .withCapabilities(caps)
+  .usingServer(`http://${process.env.SAUCE_USERNAME}:${process.env.SAUCE_ACCESS_KEY}@ondemand.saucelabs.com/wd/hub`)
+  .build()
+
+initServer((server) => {
+  driver.get('http://localhost:3000')
+
+  const h1 = driver.findElement(By.tagName('h1'))
+
+  driver.wait(until.elementTextIs(h1, 'Success'), 30000)
+  driver.quit()
+    .then(() => {
+      server.close()
+    })
+})
