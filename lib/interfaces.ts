c6628460c1b5455545cb3a3fed97ace8ff036d74
Sergii Bezliudnyi <serg.bezludny@gmail.com>
2018-06-04 10:21:50 +0200

    add core interface

---
+++ lib/interfaces.ts
@@ -0,0 +1,13 @@
+// import { AxiosInstance, AxiosStatic } from '@contentful/axios';
+
+// declare module 'contentful-sdk-core' {
+//   export function createHttpClient(
+//     axios: AxiosStatic,
+//     options: { [key: string]: any }
+//   ): AxiosInstance;
+//   export function getUserAgentHeader(
+//     sdk: string,
+//     application: string,
+//     integration: string
+//   ): string[];
+// }
