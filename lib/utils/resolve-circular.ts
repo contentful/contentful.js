a91b7faf690730c9bd7b10a3db332c56bec04452
Marco Link <marco.link@contentful.com>
2020-12-08 16:53:09 +0100

    Remove entities (#511)
    * feat(contentful-api): migrate to typescript
    
    - remove all entities wrapper methods.
    
    * remove uneccessary copy step
    
    ... and with that, no need for fast copy as prod dependency!

---
+++ lib/utils/resolve-circular.ts
@@ -0,0 +1,10 @@
+import mixinStringifySafe from '../mixins/stringify-safe'
+import resolveResponse from 'contentful-resolve-response'
+
+export function resolveCircular (data, { resolveLinks, removeUnresolved }) {
+  const wrappedData = mixinStringifySafe(data)
+  if (resolveLinks) {
+    wrappedData.items = resolveResponse(wrappedData, { removeUnresolved, itemEntryPoints: ['fields'] })
+  }
+  return wrappedData
+}
