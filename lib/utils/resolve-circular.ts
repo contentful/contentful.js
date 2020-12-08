9fbbcf80380fec9b9e78af664f75753f51637360
Marco Link <marco.link@contentful.com>
2020-12-08 16:51:05 +0100

    remove uneccessary copy step
    ... and with that, no need for fast copy as prod dependency!

--- lib/utils/resolve-circular.ts
+++ lib/utils/resolve-circular.ts
@@ -1,9 +1,8 @@
-import copy from 'fast-copy'
 import mixinStringifySafe from '../mixins/stringify-safe'
 import resolveResponse from 'contentful-resolve-response'
 
 export function resolveCircular (data, { resolveLinks, removeUnresolved }) {
-  const wrappedData = mixinStringifySafe(copy(data))
+  const wrappedData = mixinStringifySafe(data)
   if (resolveLinks) {
     wrappedData.items = resolveResponse(wrappedData, { removeUnresolved, itemEntryPoints: ['fields'] })
   }
