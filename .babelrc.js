f9a87646b3cedf19bab1aa9ef211581635872bc8
Marco Link <marco.link@contentful.com>
2020-12-09 11:01:28 +0100

    feat(chore): re-activate package version for headers (#515)

---
+++ .babelrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    '@babel/plugin-proposal-object-rest-spread',
+    [
+      'inline-replace-variables',
+      {
+        __VERSION__: require('./package.json').version
+      }
+    ]
+  ]
+}
---
+++ lib/global.d.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line no-unused-vars
+declare var __VERSION__: string;
