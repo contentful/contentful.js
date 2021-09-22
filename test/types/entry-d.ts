70c4a0196b76d9d7ebaa820a2999d2c031d65d88
Marco Link <marco.link@contentful.com>
2021-09-22 07:51:42 +0200

    add type test for entry types (not passing)

---
+++ test/types/entry-d.ts
@@ -0,0 +1,50 @@
+import { expectAssignable, expectType } from 'tsd'
+import { Entry, EntryFields, EntrySys, LocalizedEntry } from '../../lib'
+
+export const stringValue = ''
+export const numberValue = 123
+export const booleanValue = true
+export const dateValue = '2018-05-03T09:18:16.329Z'
+
+const entrySysValue: EntrySys = {
+  contentType: { sys: { id: stringValue, type: 'Link', linkType: 'ContentType' } },
+  environment: { sys: { id: stringValue, type: 'Link', linkType: 'Environment' } },
+  revision: numberValue,
+  space: { sys: { id: stringValue, type: 'Link', linkType: 'Space' } },
+  type: '', //?
+  updatedAt: dateValue,
+  id: stringValue,
+  createdAt: dateValue
+}
+
+expectAssignable<Entry<{ stringField: EntryFields.Text }>>({
+  sys: entrySysValue,
+  fields: {
+    stringField: stringValue
+  }
+})
+
+expectAssignable<LocalizedEntry<{ stringField: EntryFields.Text }, 'US' | 'DE'>>({
+  sys: entrySysValue,
+  fields: {
+    stringField: {
+      US: stringValue,
+      DE: stringValue,
+    }
+  }
+})
+
+
+/*
+expectAssignable<ResolvedEntry<{ referenceField: EntryLink }>>({
+  sys: entrySysValue,
+  fields: {
+    referenceField: {
+      sys: entrySysValue,
+      fields: {
+        stringField: stringValue
+      }
+    }
+  }
+})
+ */
