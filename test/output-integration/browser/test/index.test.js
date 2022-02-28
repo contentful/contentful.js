f75ca669d46d78bb82e3de59bcb9f93fbf686141
Andreas Egger <andreas.egger@contentful.com>
2022-02-28 15:41:26 +0100

    chore(test): Add post build user agent header test

--- test/output-integration/browser/test/index.test.js
+++ test/output-integration/browser/test/index.test.js
@@ -20,3 +20,17 @@ test('Entry has been loaded successfully', async () => {
   const text = await page.$eval('#content', (el) => el.innerHTML)
   expect(text).toEqual('nyancat')
 })
+
+test('Has correct user agent version', async () => {
+  const version = require('../../../../package.json').version
+  const clientVersion = await page.$eval('#version', (el) => el.innerHTML)
+
+  // When we make a publish run, we need to ensure that semantic-release has set a valid package version
+  if (process.env.PUBLISH_RUN === 'true') {
+    expect(clientVersion).toEqual(expect.not.stringContaining('semantic-release'))
+    expect(clientVersion).toEqual(version)
+  } else {
+    expect(clientVersion).toEqual(version)
+  }
+  console.log(`Client version: ${clientVersion}`)
+})
