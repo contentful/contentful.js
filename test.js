8f17adb00864046cd970248cb13dba292d103c2d
Alican Dedekarginoglu <alicankargin@gmail.com>
2023-01-17 16:18:19 +0100

    fix: depth and query for fetching resource links

--- test.js
+++ test.js
@@ -5,12 +5,15 @@ const client = contentful.createClient({
   // accessToken: "123"
   accessTokenBySpaceId: {
     '6fqijljzyr0e': 'foo',
-    kdtd0watvk6m: 'bar'
+    kdtd0watvk6m: 'foo', // Get your token from https://app.contentful.com/spaces/kdtd0watvk6m/api/keys/4GjyewOzLJapX3v8uhCPmT
+    wbikpgc0fqzf: 'foo', // Get your token from https://app.contentful.com/spaces/wbikpgc0fqzf/api/keys/2par7jzLTMGLSdl2X0o7Wy
+    bqct66ehcfcf: 'foo', // Get your token from https://app.contentful.com/spaces/bqct66ehcfcf/environments/master/api/keys/4mcK4YrrrSmZbJk7MB0ITH
+    pwi5ymchftec: 'foo' // Get your token https://app.contentful.com/spaces/pwi5ymchftec/environments/master/api/keys
   },
   resolveResourceLinks: true
 })
 // This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
 client
-  .getEntry('66NQogu3bNHRMDaDASSgFm', undefined, 2)
+  .getEntry('66NQogu3bNHRMDaDASSgFm', undefined, 10)
   .then(entry => console.dir(entry, { depth: 10 }))
   .catch(err => console.log(err))
