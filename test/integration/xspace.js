285ed0c423430dd6b1f11c62fc283b6393263b8b
Joanna Zakrzewska <816004+Silhoue@users.noreply.github.com>
2023-02-08 11:30:04 +0100

    test: update

--- test/integration/xspace.js
+++ test/integration/xspace.js
@@ -1,5 +1,4 @@
 import test from 'blue-tape'
-
 import * as contentful from '../../lib/contentful'
 
 /* Test data structure:
@@ -36,7 +35,6 @@ export const xspaceTests = () => {
   test('Resolves xspace link', async (t) => {
     t.plan(1)
     const article = await client.getEntry(ENTRY_ID)
-    // console.dir(article, { depth: 10 })
 
     t.ok(article.fields.author.fields)
   })
@@ -44,15 +42,21 @@ export const xspaceTests = () => {
   test('Does not resolve xspace link for include = 0', async (t) => {
     t.plan(1)
     const article = await client.getEntry(ENTRY_ID, { include: 0 })
-    // console.dir(article, { depth: 10 })
 
     t.notOk(article.fields.author.fields)
+  })
+
+  test('Resolves xspace links to two different spaces', async (t) => {
+    t.plan(2)
+    const article = await client.getEntry(ENTRY_ID)
+
+    t.ok(article.fields.author.fields)
+    t.ok(article.fields.views.fields)
   })
 
   test('Resolves local link and nested xspace link', async (t) => {
     t.plan(2)
     const article = await client.getEntry(ENTRY_ID, { include: 2 })
-    // console.dir(article, { depth: 10 })
 
     t.ok(article.fields.content.fields)
     t.ok(article.fields.content.fields.additionalInfo.fields)
@@ -61,7 +65,6 @@ export const xspaceTests = () => {
   test('Resolves xspace link and nested local link', async (t) => {
     t.plan(2)
     const article = await client.getEntry(ENTRY_ID, { include: 2 })
-    // console.dir(article, { depth: 10 })
 
     t.ok(article.fields.author.fields)
     t.ok(article.fields.author.fields.book.fields)
@@ -70,7 +73,6 @@ export const xspaceTests = () => {
   test('Resolves local link but not nested xspace link for include = 1', async (t) => {
     t.plan(2)
     const article = await client.getEntry(ENTRY_ID, { include: 1 })
-    // console.dir(article, { depth: 10 })
 
     // Article.Content.AdditionalInfo should not resolve, it is deeper than 1
     t.ok(article.fields.content.fields)
@@ -80,20 +82,9 @@ export const xspaceTests = () => {
   test('Resolves xspace link but not nested local link for include = 1', async (t) => {
     t.plan(2)
     const article = await client.getEntry(ENTRY_ID, { include: 1 })
-    // console.dir(article, { depth: 10 })
 
     // Article.Author.Book should not resolve, it is deeper than 1
     t.ok(article.fields.author.fields)
     t.notOk(article.fields.author.fields.book.fields)
-  })
-
-  test('Resolves xspace link when other nested links exist for include = 1', async (t) => {
-    t.plan(2)
-    const article = await client.getEntry(ENTRY_ID, { include: 1 })
-    // console.dir(article, { depth: 10 })
-
-    // Article.Views should resolve, not deeper than 1
-    t.ok(article.fields.views.fields)
-    t.ok(article.fields.views.fields.views.fields)
   })
 }
