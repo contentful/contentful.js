92c30b4fcda3016e08476556f6f6b176c8ed2336
Marta Sierosławska <marta.sieroslawska@contentful.com>
2023-02-16 17:23:44 +0100

    fix: remove unused import

--- test/types/asset-d.ts
+++ test/types/asset-d.ts
@@ -16,7 +16,6 @@ import {
   ConfiguredAssetCollection,
 } from '../../lib'
 import {
-  ChainOptions,
   ChainOptionWithAllLocalesAndWithLinkResolutionAndWithUnresolvableLinks,
   ChainOptionWithAllLocalesAndWithoutLinkResolution,
   ChainOptionWithAllLocalesAndWithLinkResolutionAndWithoutUnresolvableLinks,
