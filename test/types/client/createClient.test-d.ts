02e63b44a11093582eec4a0a5fa189a557b9c1ba
Marta Sierosławska <marta.sieroslawska@contentful.com>
2023-02-17 09:58:17 +0100

    fix: fix linting

--- test/types/client/createClient.test-d.ts
+++ test/types/client/createClient.test-d.ts
@@ -1,16 +1,21 @@
 import { expectType } from 'tsd'
 
-import { createClient } from '../../../lib';
-import { ClientWithAllLocalesAndWithLinkResolutionAndWithoutUnresolvableLinks, ClientWithAllLocalesAndWithLinkResolutionAndWithUnresolvableLinks, ClientWithAllLocalesAndWithoutLinkResolution, ClientWithLinkResolutionAndWithoutUnresolvableLinks, ClientWithLinkResolutionAndWithUnresolvableLinks, ClientWithoutLinkResolution } from '../../../lib/create-contentful-api';
+import { createClient } from '../../../lib'
+import {
+  ClientWithAllLocalesAndWithLinkResolutionAndWithoutUnresolvableLinks,
+  ClientWithAllLocalesAndWithLinkResolutionAndWithUnresolvableLinks,
+  ClientWithAllLocalesAndWithoutLinkResolution,
+  ClientWithLinkResolutionAndWithoutUnresolvableLinks,
+  ClientWithLinkResolutionAndWithUnresolvableLinks,
+  ClientWithoutLinkResolution,
+} from '../../../lib/create-contentful-api'
 
 const CLIENT_OPTIONS = {
   accessToken: 'accessToken',
   space: 'spaceId',
 }
 
-expectType<ClientWithLinkResolutionAndWithUnresolvableLinks>(
-  createClient(CLIENT_OPTIONS)
-)
+expectType<ClientWithLinkResolutionAndWithUnresolvableLinks>(createClient(CLIENT_OPTIONS))
 
 expectType<ClientWithAllLocalesAndWithLinkResolutionAndWithUnresolvableLinks>(
   createClient(CLIENT_OPTIONS).withAllLocales
@@ -23,9 +28,7 @@ expectType<ClientWithAllLocalesAndWithoutLinkResolution>(
 expectType<ClientWithAllLocalesAndWithLinkResolutionAndWithoutUnresolvableLinks>(
   createClient(CLIENT_OPTIONS).withAllLocales.withoutUnresolvableLinks
 )
-expectType<ClientWithoutLinkResolution>(
-  createClient(CLIENT_OPTIONS).withoutLinkResolution
-)
+expectType<ClientWithoutLinkResolution>(createClient(CLIENT_OPTIONS).withoutLinkResolution)
 
 expectType<ClientWithLinkResolutionAndWithoutUnresolvableLinks>(
   createClient(CLIENT_OPTIONS).withoutUnresolvableLinks
