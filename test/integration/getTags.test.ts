08783c0975daaec536bff97341267c41be578a8c
Cyberxon <marouenbs93@gmail.com>
2023-03-15 17:59:30 +0100

    fix: code review

--- test/integration/getTags.test.ts
+++ test/integration/getTags.test.ts
@@ -8,11 +8,9 @@ if (process.env.API_INTEGRATION_TESTS) {
 
 const client = contentful.createClient(params)
 
-describe.only('getTags', () => {
+describe('getTags', () => {
   it('returns all tags when no filters are available', async () => {
     const response = await client.getTags()
-
-    console.dir(response, { depth: null })
 
     expect(response.items[0].sys.type).toBe('Tag')
   })
@@ -21,42 +19,42 @@ describe.only('getTags', () => {
     it('gets the tag with the name equals to the provided value', async () => {
       const response = await client.getTags({ name: 'public tag 1' })
 
-      expect(response.items.length).toBe(1)
+      expect(response.items).toHaveLength(1)
       expect(response.items[0].name).toEqual('public tag 1')
     })
 
     it('gets the tag with the name not equals to the provided value', async () => {
       const response = await client.getTags({ 'name[ne]': 'public tag 1' })
 
-      expect(response.items.length).toBe(0)
+      expect(response.items).toHaveLength(0)
       expect(response.items).toEqual([])
     })
 
     it('gets the tags with the name matching to the provided value', async () => {
       const response = await client.getTags({ 'name[match]': 'public tag' })
 
-      expect(response.items.length).toBe(1)
+      expect(response.items).toHaveLength(1)
       expect(response.items[0].name).toEqual('public tag 1')
     })
 
     it('gets the tags with the name in the list of the provided value', async () => {
       const response = await client.getTags({ 'name[in]': 'public tag,public tag 1' })
 
-      expect(response.items.length).toBe(1)
+      expect(response.items).toHaveLength(1)
       expect(response.items[0].name).toEqual('public tag 1')
     })
 
     it('gets the tags with the name not in the list of the provided value', async () => {
       const response = await client.getTags({ 'name[nin]': 'public tag,public tag 1' })
 
-      expect(response.items.length).toBe(0)
+      expect(response.items).toHaveLength(0)
       expect(response.items).toEqual([])
     })
 
     it('gets the tags with the name exists', async () => {
       const response = await client.getTags({ 'name[exists]': true })
 
-      expect(response.items.length).toBe(1)
+      expect(response.items).toHaveLength(1)
       expect(response.items[0].name).toEqual('public tag 1')
     })
   })
@@ -65,35 +63,35 @@ describe.only('getTags', () => {
     it('can filter by id', async () => {
       const response = await client.getTags({ 'sys.id': 'publicTag1' })
 
-      expect(response.items.length).toBe(1)
+      expect(response.items).toHaveLength(1)
       expect(response.items[0].sys.id).toEqual('publicTag1')
     })
 
     it('can filter by createdAt', async () => {
       const response = await client.getTags({ 'sys.createdAt': '2021-02-11T14:44:48.594Z' })
 
-      expect(response.items.length).toBe(1)
+      expect(response.items).toHaveLength(1)
       expect(response.items[0].sys.id).toEqual('publicTag1')
     })
 
     it('can filter by updateAt', async () => {
       const response = await client.getTags({ 'sys.updatedAt': '2021-02-11T14:44:48.594Z' })
 
-      expect(response.items.length).toBe(1)
+      expect(response.items).toHaveLength(1)
       expect(response.items[0].sys.id).toEqual('publicTag1')
     })
 
     it('can filter by visibility', async () => {
       const response = await client.getTags({ 'sys.visibility': 'private' })
 
-      expect(response.items.length).toBe(0)
+      expect(response.items).toHaveLength(0)
       expect(response.items).toEqual([])
     })
 
     it('can filter by type', async () => {
       const response = await client.getTags({ 'sys.type': 'Tag' })
 
-      expect(response.items.length).toBe(1)
+      expect(response.items).toHaveLength(1)
       expect(response.items[0].sys.id).toEqual('publicTag1')
     })
   })
