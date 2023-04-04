034cda61ed4ec039abe7b51779b2338c85401008
Marta Sierosławska <msieroslawska@users.noreply.github.com>
2023-04-04 14:58:21 +0200

    feat: add missing exports for queries to supress typedoc warnings

--- lib/types/query/index.ts
+++ lib/types/query/index.ts
@@ -1,8 +1,41 @@
-export {
-  type AssetQueries,
-  type AssetsQueries,
-  type EntryQueries,
-  type EntriesQueries,
-  type TagQueries,
-} from './query'
 export { type FieldsType, EntrySkeletonType } from './util'
+
+export type {
+  EqualityFilter,
+  EntryFieldsEqualityFilter,
+  InequalityFilter,
+  EntryFieldsInequalityFilter,
+} from './equality'
+export type { ExistenceFilter, EntryFieldsExistenceFilter } from './existence'
+export type { ProximitySearchFilter, LocationSearchFilters } from './location'
+export type {
+  EntryOrderFilterWithFields,
+  EntryOrderFilter,
+  AssetOrderFilter,
+  TagOrderFilter,
+} from './order'
+export type {
+  AssetFieldsQueries,
+  AssetFieldsFileQueries,
+  AssetFieldsFileDetailsQueries,
+  AssetQueries,
+  AssetsQueries,
+  EntryContentTypeQuery,
+  EntryFieldsQueries,
+  EntryQueries,
+  EntriesQueries,
+  MetadataTagsQueries,
+  TagQueries,
+  SysQueries,
+} from './query'
+export type { RangeFilters, EntryFieldsRangeFilters } from './range'
+export type { ReferenceSearchFilters } from './reference'
+export type { FullTextSearchFilters, EntryFieldsFullTextSearchFilters } from './search'
+export type {
+  SelectFilterPaths,
+  EntrySelectFilterWithFields,
+  EntrySelectFilter,
+  AssetSelectFilter,
+} from './select'
+export type { EntryFieldsSetFilter } from './set'
+export type { SubsetFilters, EntryFieldsSubsetFilters } from './subset'
