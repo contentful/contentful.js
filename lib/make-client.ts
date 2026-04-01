8f0cd5f3b1537fd0b1ad5d830a94d5b280bdfef0
Ethan Ozelius <ethan.ozelius@contentful.com>
2026-04-01 13:28:39 -0600

    initial commit to add an error boundary wrapper to async methods

--- lib/make-client.ts
+++ lib/make-client.ts
@@ -7,6 +7,70 @@ import type {
   ModifiersFromOptions,
 } from './utils/client-helpers.js'
 import type { ContentfulClientApi } from './types/index.js'
+import type { ChainModifiers } from './types/client.js'
+
+/**
+ * Wraps an async method with error handling to invoke logHandler on errors
+ */
+function wrapAsyncMethod(
+  method: (...args: any[]) => Promise<any>,
+  logHandler: (level: string, data: any) => void,
+): (...args: any[]) => Promise<any> {
+  return async (...args: any[]) => {
+    try {
+      return await method(...args)
+    } catch (error) {
+      logHandler('error', error)
+      throw error
+    }
+  }
+}
+
+/**
+ * Creates an error boundary around all async API methods in the client
+ */
+function withErrorBoundary(
+  client: ContentfulClientApi<any>,
+  logHandler: (level: string, data: any) => void,
+): ContentfulClientApi<any> {
+  // List of all async methods that should be wrapped with error handling
+  // Note: parseEntries is NOT async, so it's not included
+  const asyncMethods = [
+    'getSpace',
+    'getContentType',
+    'getContentTypes',
+    'getAsset',
+    'getAssets',
+    'getAssetsWithCursor',
+    'getTag',
+    'getTags',
+    'getLocales',
+    // 'parseEntries',
+    'sync',
+    'getEntry',
+    'getEntries',
+    'getEntriesWithCursor',
+    'getConceptScheme',
+    'getConceptSchemes',
+    'getConcept',
+    'getConcepts',
+    'getConceptAncestors',
+    'getConceptDescendants',
+    'createAssetKey',
+  ] as const
+
+  const wrappedMethods: any = {}
+
+  for (const methodName of asyncMethods) {
+    const originalMethod = client[methodName as keyof ContentfulClientApi<any>]
+    if (typeof originalMethod === 'function') {
+      wrappedMethods[methodName] = wrapAsyncMethod(originalMethod as any, logHandler)
+    }
+  }
+
+  // Return client with wrapped methods, preserving non-wrapped methods
+  return Object.assign({}, client, wrappedMethods) as ContentfulClientApi<any>
+}
 
 function create<OptionsType extends ChainOptions>(
   { http, getGlobalOptions }: CreateContentfulApiParams,
@@ -20,21 +84,24 @@ function create<OptionsType extends ChainOptions>(
     },
     options,
   )
-  const response: any = client ? client : {}
 
-  Object.defineProperty(response, 'withAllLocales', {
+  // Apply error boundary to catch and log errors from all API methods
+  const logHandler = http.defaults.logHandler
+  const clientWithErrorBoundary = withErrorBoundary(client, logHandler) as any
+
+  Object.defineProperty(clientWithErrorBoundary, 'withAllLocales', {
     get: () => makeInnerClient({ ...options, withAllLocales: true }),
   })
-  Object.defineProperty(response, 'withoutLinkResolution', {
+  Object.defineProperty(clientWithErrorBoundary, 'withoutLinkResolution', {
     get: () => makeInnerClient({ ...options, withoutLinkResolution: true }),
   })
-  Object.defineProperty(response, 'withoutUnresolvableLinks', {
+  Object.defineProperty(clientWithErrorBoundary, 'withoutUnresolvableLinks', {
     get: () => makeInnerClient({ ...options, withoutUnresolvableLinks: true }),
   })
-  Object.defineProperty(response, 'withLocaleBasedPublishing', {
+  Object.defineProperty(clientWithErrorBoundary, 'withLocaleBasedPublishing', {
     get: () => makeInnerClient({ ...options, withLocaleBasedPublishing: true }),
   })
-  return Object.create(response) as ContentfulClientApi<ModifiersFromOptions<OptionsType>>
+  return clientWithErrorBoundary as ContentfulClientApi<ModifiersFromOptions<OptionsType>>
 }
 
 export const makeClient = ({
@@ -57,8 +124,11 @@ export const makeClient = ({
     },
   )
 
-  return {
-    ...client,
+  // Apply error boundary to the main client
+  const logHandler = http.defaults.logHandler
+  const clientWithErrorBoundary = withErrorBoundary(client, logHandler)
+
+  return Object.assign(clientWithErrorBoundary, {
     get withAllLocales() {
       return makeInnerClient<ChainOption<'WITH_ALL_LOCALES'>>({
         withAllLocales: true,
@@ -91,5 +161,5 @@ export const makeClient = ({
         withLocaleBasedPublishing: true,
       })
     },
-  }
+  })
 }
